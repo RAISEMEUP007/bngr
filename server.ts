3dbcfdb46f8551caf139cc696f5ad3aefd95bff7
AnttoLion <anttolion@gmail.com>
2024-08-02 09:14:44 -0400

    Use collections

--- server.ts
+++ server.ts
@@ -31,65 +31,74 @@ router.use("/getforecastdata/", async (req, res, next) => {
       throw new Error('Failed to fetch data');
     }
 
-    const venueList = await response.json();
+    const collection = await response.json();
+      console.log(collection);
     const poppin = []
     const hot = []
     const dead = []
-    for (const venue of venueList) {
+    for (const venue_id of collection.venue_ids) {
       const params = new URLSearchParams({
         'api_key_private': `${process.env.BESTTIME_PRIVATE_KEY}`,
-        'venue_id': venue.venue_id
+        'venue_id': venue_id
       });
 
       const res = await fetch(`https://besttime.app/api/v1/forecasts/live?${params}`, {
         method: 'POST'
       });
-
       if (!res.ok) {
         throw new Error('Failed to fetch data');
       }
 
       const data = await res.json();
+      console.log(data);
       if(data.analysis.venue_forecasted_busyness >= 100 && data.analysis.venue_live_busyness >= 100){
         poppin.push({
-          venue_id: venue.venue_id,
-          venue_name: venue.venue_name,
+          venue_id: venue_id,
+          venue_name: data.venue_info.venue_name,
           forecast: 'Very busy',
           Acutal: `Extremely busy (${data.analysis.venue_live_busyness}%)`,
           Actual_value: data.analysis.venue_live_busyness
         })
       }else if(data.analysis.venue_forecasted_busyness < 100 && data.analysis.venue_live_busyness >= 150){
         poppin.push({
-          venue_id: venue.venue_id,
-          venue_name: venue.venue_name,
+          venue_id: venue_id,
+          venue_name: data.venue_info.venue_name,
           forecast: 'Not busy',
           Acutal: `Extremely busy (${data.analysis.venue_live_busyness}%)`,
           Actual_value: data.analysis.venue_live_busyness
         })
       }else if(data.analysis.venue_forecasted_busyness >= 100 && data.analysis.venue_live_busyness < 100){
         hot.push({
-          venue_id: venue.venue_id,
-          venue_name: venue.venue_name,
+          venue_id: venue_id,
+          venue_name: data.venue_info.venue_name,
           forecast: 'Very busy',
           Acutal: `Not busy (${data.analysis.venue_live_busyness}%)`,
           Actual_value: data.analysis.venue_live_busyness
         })
       }else if(data.analysis.venue_forecasted_busyness < 100 && data.analysis.venue_live_busyness < 150){
         dead.push({
-          venue_id: venue.venue_id,
-          venue_name: venue.venue_name,
+          venue_id: venue_id,
+          venue_name: data.venue_info.venue_name,
           forecast: 'Dead',
           Acutal: `Dead (${data.analysis.venue_live_busyness}%)`,
           Actual_value: data.analysis.venue_live_busyness
+        })
+      }else if(data.analysis.venue_live_busyness_available === false){
+        dead.push({
+          venue_id: venue_id,
+          venue_name: data.venue_info.venue_name,
+          forecast: 'Dead',
+          Acutal: `Dead (0%)`,
+          Actual_value: 0
         })
       }
       // forecastData.push(data);
     }
 
     const forecasts = {
-      poppin,
-      hot,
-      dead,
+      poppin: poppin.sort((a, b)=>b.Actual_value - a.Actual_value),
+      hot: hot.sort((a, b)=>b.Actual_value - a.Actual_value),
+      dead: dead.sort((a, b)=>b.Actual_value - a.Actual_value),
     }
 
     res.status(200).json(forecasts);
